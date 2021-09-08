fd0d99ee9607d3ed94f605cf3a48746a875abb3e
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2021-09-08 12:54:39 +0200

    Implementing getters for items

--- src/klausurarchiv/model.py
+++ src/klausurarchiv/model.py
@@ -1,46 +1,69 @@
 from pathlib import Path
 from datetime import date
+from uuid import UUID
+import os
+import shutil
+import json
+
+CONFIG_FILENAME = Path(".config")
 
 
 class Document(object):
+    def __init__(self, path: Path):
+        self.__path = path
+
     @property
     def path(self) -> Path:
-        pass
+        return self.__path
 
 
 class Item(object):
-    def __init__(self):
-        pass
+    def __init__(self, path: Path):
+        self.__path = path
+        assert (self.__path.is_dir())
+        assert ((self.__path / CONFIG_FILENAME).exists())
+
+    def __get_config(self) -> dict:
+        config_path = self.path / CONFIG_FILENAME
+        return json.load(open(config_path, mode="r"))
 
     @property
-    def downloadable(self) -> bool: 
-        pass
+    def uuid(self) -> UUID:
+        return UUID(self.path.name.partition(" ")[0])
+
+    @property
+    def downloadable(self) -> bool:
+        return self.__get_config()["downloadable"]
 
     @property
     def path(self) -> Path:
-        pass
+        return self.__path
 
     @property
     def name(self) -> str:
-        pass
+        return self.path.name.partition(" ")[2]
 
     @property
     def date(self) -> date:
-        pass
+        return date.fromisoformat(self.__get_config()["date"])
 
     @property
     def author(self) -> str:
-        pass
+        return self.__get_config()["author"]
 
     @property
     def documents(self) -> list[Document]:
-        pass
+        return [Document(doc_path) for doc_path in self.path.iterdir() if doc_path.name != CONFIG_FILENAME]
 
     def add_document(self, original_path: Path) -> Document:
-        pass
+        target_path = self.path / original_path.name
+        shutil.copy(original_path, target_path)
+        return Document(target_path)
 
     def remove_document(self, document: Document):
-        pass
+        if document.path.parent != self.path:
+            raise KeyError(f"Document {document} is not part of item {self}")
+        os.remove(document.path)
 
 
 class Subject(object):
@@ -56,9 +79,6 @@ class Subject(object):
 
 
 class Archive(object):
-    def __init__(self, base_path: Path):
-        self.__base_path = base_path
-
     @property
     def items(self) -> list[Item]:
         pass
