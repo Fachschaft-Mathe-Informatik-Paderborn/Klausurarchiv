b8a74fde3f2afb0ca24b97c1dd96cca9cd94b021
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2021-09-08 15:20:22 +0200

    Introducing the ItemMeta class/mechanic

--- src/klausurarchiv/model.py
+++ src/klausurarchiv/model.py
@@ -1,9 +1,10 @@
-from pathlib import Path
 import datetime
-from uuid import UUID
+import json
 import os
 import shutil
-import json
+from pathlib import Path
+from typing import Optional
+from uuid import UUID, uuid4
 
 META_FILENAME = Path("meta.json")
 
@@ -16,44 +17,53 @@ class Document(object):
     def path(self) -> Path:
         return self.__path
 
+    def __str__(self) -> str:
+        return str(self.path.parent / self.path.name)
+
+
+class ItemMeta(object):
+    def __init__(self):
+        self.downloadable: bool = False
+        self.date: Optional[datetime.date] = None
+        self.author: Optional[str] = None
+
 
 class Item(object):
     def __init__(self, path: Path):
         self.__path = path
         assert (self.__path.is_dir())
-        assert (self.__get_config_path().exists())
+        assert (self.__meta_path.exists())
 
-    def __get_config_path(self):
+    @property
+    def __meta_path(self):
         return self.__path / META_FILENAME
 
-    def __get_config(self) -> dict:
-        config_path = self.__get_config_path()
-        with open(config_path, mode="r") as file:
-            config = json.load(file)
-        return config
-
-    def __set_config_field(self, key: str, value: str):
-        config = self.__get_config()
-        config[key] = value
-        config_path = self.__get_config_path()
-        with open(config_path, mode="w") as file:
-            json.dump(config, file)
-
     @property
-    def uuid(self) -> UUID:
-        return UUID(self.path.name.partition(" ")[0])
+    def __meta(self) -> ItemMeta:
+        meta = ItemMeta()
+        with open(self.__meta_path, mode="r") as file:
+            meta.__dict__ = json.load(file)
+        return meta
 
-    @property
-    def downloadable(self) -> bool:
-        return self.__get_config()["downloadable"]
+    @__meta.setter
+    def __meta(self, new_meta: ItemMeta):
+        with open(self.__meta_path, mode="w") as file:
+            json.dump(new_meta.__dict__, file)
 
-    @downloadable.setter
-    def downloadable(self, new_value: bool):
-        self.__set_config_field("downloadable", str(new_value))
+    @staticmethod
+    def new_item(base_dir: Path) -> 'Item':
+        uuid = uuid4()
+        name = "unnamed"
+        item_path = base_dir / Path(f"{uuid} {name}")
+        os.mkdir(item_path)
 
     @property
     def path(self) -> Path:
         return self.__path
+
+    @property
+    def uuid(self) -> UUID:
+        return UUID(self.path.name.partition(" ")[0])
 
     @property
     def name(self) -> str:
@@ -67,29 +77,43 @@ class Item(object):
         self.__path = new_path
 
     @property
-    def date(self) -> datetime.date:
-        return datetime.date.fromisoformat(self.__get_config()["date"])
+    def downloadable(self) -> bool:
+        return self.__meta.downloadable
 
-    @date.setter
-    def date(self, new_date: date):
-        self.__set_config_field("date", new_date.isoformat())
+    @downloadable.setter
+    def downloadable(self, new_value: bool):
+        meta = self.__meta
+        meta.downloadable = new_value
+        self.__meta = meta
 
     @property
-    def author(self) -> str:
-        return self.__get_config()["author"]
+    def date(self) -> Optional[datetime.date]:
+        return self.__meta.date
+
+    @date.setter
+    def date(self, new_date: Optional[datetime.date]):
+        meta = self.__meta
+        meta.date = new_date
+        self.__meta = meta
+
+    @property
+    def author(self) -> Optional[str]:
+        return self.__meta.author
 
     @author.setter
     def author(self, new_author: str):
-        self.__set_config_field("author", new_author)
+        meta = self.__meta
+        meta.author = new_author
+        self.__meta = meta
 
     @property
     def documents(self) -> list[Document]:
         return [Document(doc_path) for doc_path in self.path.iterdir() if doc_path.name != META_FILENAME]
 
     def add_document(self, original_path: Path) -> Document:
-        target_path = self.path / original_path.name
         if original_path.name == META_FILENAME:
             raise KeyError(f"Documents may not have the name {META_FILENAME}")
+        target_path = self.path / original_path.name
         shutil.copy(original_path, target_path)
         return Document(target_path)
 
@@ -100,11 +124,21 @@ class Item(object):
             raise KeyError(f"{META_FILENAME} is not a document")
         os.remove(document.path)
 
+    def __str__(self) -> str:
+        return str(self.path.name)
+
 
 class Subject(object):
+    def __init__(self, path: Path):
+        self.__path = path
+
+    @property
+    def path(self) -> Path:
+        return self.__path
+
     @property
     def items(self) -> list[Item]:
-        pass
+        return [Item(item_path) for item_path in self.path.iterdir() if item_path.is_dir()]
 
     def add_item(self, item: Item):
         pass
