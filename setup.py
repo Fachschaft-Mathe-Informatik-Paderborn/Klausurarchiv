83ed7b12569b4f7397563466652c654948454bd2
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-08-03 19:24:46 +0200

    Lowering the version of flask_caching

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "flask_cors~=3.0.10",
         "flask_login~=0.6.2",
         "requests~=2.30.0",
-        "flask_caching~=2.0.2",
+        "flask_caching~=2.0.0",
         "flask_SQLAlchemy~=3.0.3",
         "flask_marshmallow~=0.15.0",
         "marshmallow-sqlalchemy~=0.29.0",
