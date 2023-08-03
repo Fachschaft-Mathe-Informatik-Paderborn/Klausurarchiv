4daa720cc0392d8148f02a12bf619281f755b8c1
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-08-03 19:26:03 +0200

    Even lower versions

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     package_dir={'': 'src'},
     install_requires=[
         "flask~=2.3.2",
-        "flask_cors~=3.0.10",
-        "flask_login~=0.6.2",
+        "flask_cors~=3.0.0",
+        "flask_login~=0.6.0",
         "requests~=2.30.0",
         "flask_caching~=2.0.0",
-        "flask_SQLAlchemy~=3.0.3",
+        "flask_SQLAlchemy~=3.0.0",
         "flask_marshmallow~=0.15.0",
         "marshmallow-sqlalchemy~=0.29.0",
-        "psycopg2~=2.9.5"
+        "psycopg2~=2.9.0"
     ]
 )
