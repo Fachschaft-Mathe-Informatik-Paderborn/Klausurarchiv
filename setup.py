1f639a61b74c243be076306dd0c710328d339416
Jan-Oliver Opdenhövel <joo@mail.uni-paderborn.de>
2023-08-03 19:31:28 +0200

    Dropping semantic versions entirely

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(
     packages=find_packages(where="src"),
     package_dir={'': 'src'},
     install_requires=[
-        "flask~=2.3.2",
-        "flask_cors~=3.0.0",
-        "flask_login~=0.6.0",
-        "requests~=2.30.0",
-        "flask_caching~=2.0.0",
-        "flask_SQLAlchemy~=3.0.0",
-        "flask_marshmallow~=0.15.0",
-        "marshmallow-sqlalchemy~=0.29.0",
-        "psycopg2~=2.9.0"
+        "flask",
+        "flask_cors",
+        "flask_login",
+        "requests",
+        "flask_caching",
+        "flask_SQLAlchemy",
+        "flask_marshmallow",
+        "marshmallow-sqlalchemy",
+        "psycopg2"
     ]
 )
